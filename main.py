3bc7acdf20e30e932ba1666199edbab0803dba2a
Tobitantin <csheroff@comcast.net>
2025-08-26 12:07:48 -0400

    60 fps

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +15,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
     print("Starting Asteroids!")
     print("Screen width: 1280")
