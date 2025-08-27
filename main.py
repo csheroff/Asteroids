f0751381622f4e0460dc6987ae89b75748262aa9
Tobitantin <csheroff@comcast.net>
2025-08-27 13:47:28 -0400

    rotation

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
+        
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
